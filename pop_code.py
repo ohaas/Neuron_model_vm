883cde87a6232289e666c45de93af7fe08cd4c37
Olivia Haas <haasolivia@gmail.com>
2012-03-20 17:17:26 +0100

    Stimulus import

--- pop_code.py
+++ pop_code.py
@@ -2,6 +2,14 @@ __author__ = 'ohaas'
 
 import numpy as ny
 import matplotlib.pyplot as pp
+import stimulus
 
 class Population(object):
     pass
+
+    a= stimulus.stimulus(30)
+    b= a.square(6,3)
+
+    pp.imshow(b)
+    pp.show()
+
