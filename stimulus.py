f6dcd68f75263264bac18d90c5abaf803bc7f772
Olivia Haas <haasolivia@gmail.com>
2012-03-20 13:40:22 +0100

    black square generation

---
+++ stimulus.py
@@ -0,0 +1,17 @@
+__author__ = 'ohaas'
+
+import numpy as ny
+import matplotlib.pyplot as pp
+
+class stimulus(object):
+      pass
+
+
+def __init__(self, num):
+    self.background=ny.ones((num, num, 3))
+
+def square(self, size):
+    
+
+
+pic = stimulus(30)
