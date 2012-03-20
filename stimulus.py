ce16819d692209218d9e995b099c9eedfb4ed04e
Olivia Haas <haasolivia@gmail.com>
2012-03-20 15:09:45 +0100

    show square

--- stimulus.py
+++ stimulus.py
@@ -11,7 +11,15 @@ def __init__(self, num):
     self.background=ny.ones((num, num, 3))
 
 def square(self, size):
-    
+    for s in ny.arange(2,2+size):
+        self.background[2,s]=ny.zeros(3)
+        self.background[s,2]=ny.zeros(3)
+        self.background[2+size,s]=ny.zeros(3)
+        self.background[s,2+size]=ny.zeros(3)
+    return pp.imshow(self.background)
 
-
+#stimulus(num), pic.square(size)
 pic = stimulus(30)
+print pic.square(4)
+
+
