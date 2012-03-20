291c35611b608fb3323d714262c29007d11c05c6
Olivia Haas <haasolivia@gmail.com>
2012-03-20 15:45:17 +0100

    first progress

--- stimulus.py
+++ stimulus.py
@@ -7,19 +7,18 @@ class stimulus(object):
       pass
 
 
-def __init__(self, num):
-    self.background=ny.ones((num, num, 3))
+      def __init__(self,main):
+          self.main=ny.ones((main, main, 3))
+          #return pp.imshow(self.main)
 
-def square(self, size):
-    for s in ny.arange(2,2+size):
-        self.background[2,s]=ny.zeros(3)
-        self.background[s,2]=ny.zeros(3)
-        self.background[2+size,s]=ny.zeros(3)
-        self.background[s,2+size]=ny.zeros(3)
-    return pp.imshow(self.background)
+      def square(self, size):
+          for s in ny.arange(2,2+size):
+              self.main[2,s]=ny.zeros(3)
+              self.main[s,2]=ny.zeros(3)
+              self.main[2+size,s]=ny.zeros(3)
+              self.main[s,2+size]=ny.zeros(3)
+          return pp.imshow(self.main)
 
-#stimulus(num), pic.square(size)
+#stimulus(main), pic.square(size)
 pic = stimulus(30)
 print pic.square(4)
-
-
