06036f7d4785da07066eca0f186452e554e725da
Olivia Haas <haasolivia@gmail.com>
2012-03-21 10:11:29 +0100

    color info reduced from 3 to 1 (black or white) info

--- stimulus.py
+++ stimulus.py
@@ -8,18 +8,18 @@ class stimulus(object):
 
 
       def __init__(self,main):
-          self.main=ny.ones((main, main, 3))
+          self.main=ny.ones((main, main, 1))
 
       def square(self, size, start):
           for s in ny.arange(start,start+1+size):
-              self.main[start,s]=ny.zeros(3)
-              self.main[s,start]=ny.zeros(3)
-              self.main[start+size,s]=ny.zeros(3)
-              self.main[s,start+size]=ny.zeros(3)
+              self.main[start,s]=ny.zeros(1)
+              self.main[s,start]=ny.zeros(1)
+              self.main[start+size,s]=ny.zeros(1)
+              self.main[s,start+size]=ny.zeros(1)
           return self.main
 
 #stimulus(main size of stimulus picture), pic.square(size,starting point)
-background = stimulus(30)
-pic = background.square(4,2)
-pp.imshow(pic)
-pp.show()
+#background = stimulus(30)
+#pic = background.square(4,2)
+#pp.imshow(pic)
+#pp.show()
