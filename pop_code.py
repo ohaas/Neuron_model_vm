62b2c4e951c704ee8336e7bd04692665dc4a67d5
Olivia Haas <haasolivia@gmail.com>
2012-03-21 14:28:33 +0100

    can create stimulus I, can print it and any wanted (x,y) pixel value

--- pop_code.py
+++ pop_code.py
@@ -1,15 +1,25 @@
 __author__ = 'ohaas'
 
-import numpy as ny
-import matplotlib.pyplot as pp
+#import numpy as ny
+#import matplotlib.pyplot as pp
 import stimulus
 
 class Population(object):
     pass
 
-    a= stimulus.stimulus(30)
-    b= a.square(6,3)
 
-    pp.imshow(b)
-    pp.show()
+#---- TO CREATE IMAGE STIMULUS (I) WITH IMAGE SIZE 30, STIMULUS SIZE 6 AND START POINT 2
+#    (ALL NUMBERS ARE USED IN A SQUARED FASHION, E.G. IMAGE SIZE 30 MEANS: 30x30)-----------------
+#
+
+I = stimulus.image(30,6,2)
+
+#---- TO SHOW THE STIMULUS IMAGE (I):-------------------------------------------------------------
+
+stimulus.image.show_im(I)
+
+#---- TO PRINT A PIXEL VALUE OF THE STIMULUS IMAGE (I) AT (X,Y)=(5,6)----------------------------
+
+print stimulus.image.pix_value(I,5,6)
+
 
