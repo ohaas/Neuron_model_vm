1d06319ac12dfb14beb80f70f403b5790abc6700
Olivia Haas <haasolivia@gmail.com>
2012-03-22 12:23:47 +0100

    periodicity is now in both directions ok

--- neuron.py
+++ neuron.py
@@ -11,11 +11,12 @@ class N(object):
 # AND A IS THE AMPLITUDE
 
     def __init__(self, mu, sigma, x1 , A):
-        self.y=ny.zeros(360.0)
-        x2=ny.arange( 0.0, 360.0, 1 )
+        self.y=ny.zeros(361.0)
+        x2=ny.arange( 0.0, 361.0, 1 )
         for x in x2:
-            self.y[x]= A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-mu)**2/(2.0*sigma**2)))+A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-(mu-360))**2/(2.0*sigma**2)))
+            self.y[x]= A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*(ny.exp(-(x-mu)**2/(2.0*sigma**2))+ny.exp(-(x-(mu-360))**2/(2.0*sigma**2))+ny.exp(-(x-(mu+360))**2/(2.0*sigma**2))))
         print self.y[x1]
+
         pp.plot(x2,self.y)
         pp.xlim(0,360)
         pp.xlabel('Spatial orientation in degree')
@@ -23,4 +24,5 @@ class N(object):
         pp.show()
 
 
-N(350.0,10.0, 0.0, 1)
+
+N(10.0,10.0, 0.0, 1)
