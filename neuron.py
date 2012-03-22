9a7642a2bee1e6f23120d08d5f47eea6968af7f9
Olivia Haas <haasolivia@gmail.com>
2012-03-22 10:28:53 +0100

    periodic gaussian works :)

--- neuron.py
+++ neuron.py
@@ -12,8 +12,14 @@ def gauss(mu, sigma, x , A) :
     return A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-mu)**2/(2.0*sigma**2)))
 
 # mu IS WHERE THE MAXIMUM IS LOCATED, SIGMA SQUARED IS THE WIDTH AND A IS THE AMPLITUDE
+max=350
+width=10.0
+A=1
+
 x = ny.arange( 0.0, 360.0, 0.1 )
-y = gauss( 260.0, 10.0, x , 1)
+y1 = gauss( max, width, x , A)
+y2 = gauss((max-360) , width, x, A)
+y = y1+y2
 pp.plot(x,y)
 pp.xlim(0,360)
 pp.xlabel('Spatial orientation in degree')
