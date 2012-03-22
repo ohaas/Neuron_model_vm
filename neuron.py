7e4e2508bcf795ff0255f1fc610ee548b9a5464e
Olivia Haas <haasolivia@gmail.com>
2012-03-22 11:30:31 +0100

    callable neuron with wished parameters and possible activity read out at a certain degree point x1

--- neuron.py
+++ neuron.py
@@ -4,24 +4,23 @@ import numpy as ny
 import matplotlib.pyplot as pp
 from matplotlib.mlab import bivariate_normal
 
-class Neuron(object):
+class N(object):
     pass
 
+# mu IS WHERE THE MAXIMUM IS LOCATED, SIGMA SQUARED IS THE WIDTH, x1 IS THE POSITION IN DEGREES OF THE READ OUT ACTIVITY VALUE
+# AND A IS THE AMPLITUDE
 
-def gauss(mu, sigma, x , A) :
-    return A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-mu)**2/(2.0*sigma**2)))
+    def __init__(self, mu, sigma, x1 , A):
+        self.y=ny.zeros(360.0)
+        x2=ny.arange( 0.0, 360.0, 1 )
+        for x in x2:
+            self.y[x]= A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-mu)**2/(2.0*sigma**2)))+A*25.0*((1.0/(sigma*ny.sqrt(2*ny.pi)))*ny.exp(-(x-(mu-360))**2/(2.0*sigma**2)))
+        print self.y[x1]
+        pp.plot(x2,self.y)
+        pp.xlim(0,360)
+        pp.xlabel('Spatial orientation in degree')
+        pp.ylabel('Amplitude of neuronal activation')
+        pp.show()
 
-# mu IS WHERE THE MAXIMUM IS LOCATED, SIGMA SQUARED IS THE WIDTH AND A IS THE AMPLITUDE
-max=350
-width=10.0
-A=1
 
-x = ny.arange( 0.0, 360.0, 0.1 )
-y1 = gauss( max, width, x , A)
-y2 = gauss((max-360) , width, x, A)
-y = y1+y2
-pp.plot(x,y)
-pp.xlim(0,360)
-pp.xlabel('Spatial orientation in degree')
-pp.ylabel('Amplitude of neuronal activation')
-pp.show()
+N(350.0,10.0, 0.0, 1)
