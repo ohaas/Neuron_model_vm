4e891c14c89966ed4ae7ae801d8677771c54e312
Olivia Haas <ohaas@wachtler9.cns.bzm>
2012-04-04 14:24:54 +0200

    Separate model config

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ import pop_code as pc
 import neuron
 from matplotlib.mlab import bivariate_normal
 from scipy.signal import convolve2d
+import ConfigParser as cp
 
-def get_gauss_kernel(sigma, size=10, res=1):
+def get_gauss_kernel(sigma, size=7, res=0.5):
     """
     return a two dimesional gausian kernel of shape (size*(1/resolution),size*(1/resolution))
     with a std deviation of std
@@ -48,22 +49,35 @@ class Stage(object):
 
 class Model(object):
 
-    def __init__(self, main_size, square_size, start, gauss_width, time_frames, delta_t):
-        self.main_size = main_size
-        self.square_size = square_size
-        self.gauss_width = gauss_width
-        self.time_frames = time_frames
-        self.delta_t = delta_t
-        self.start = start
+    def __init__(self, cfg_file):
 
+        cfg = cp.RawConfigParser()
+
+        cfg.read(cfg_file)
+
+        self.main_size = cfg.getint('Input', 'main_size')
+        self.square_size = cfg.getint('Input', 'square_size')
+        self.time_frames = cfg.getint('Input', 'time_frames')
+        self.delta_t = cfg.getint('Input', 'delta_t')
+        self.start = cfg.getint('Input', 'start')
+
+        self.gauss_width = cfg.getfloat('PopCode', 'neuron_sigma')
+
+        v1_C = cfg.getint('V1', 'C')
+        mt_kernel_sigma =  cfg.getfloat('MT', 'kernel_sigma')
+        mt_kernel_size =  cfg.getfloat('MT', 'kernel_size')
+        mt_kernel_res =  cfg.getfloat('MT', 'kernel_res')
+
+        self.V1 = Stage([], v1_C)
+        self.MT = Stage(get_gauss_kernel(mt_kernel_sigma, mt_kernel_size, mt_kernel_res), 0)
 
     def create_input(self):
         """
         definition of initial population codes for different time steps (is always the same one!!!)
         """
-        pop_code=ny.zeros((self.main_size, self.main_size, 8, self.time_frames/self.delta_t))
+        pop_code=ny.zeros((self.main_size, self.main_size, 8, self.time_frames))
         start = self.start
-        for i in ny.arange(0, self.time_frames/self.delta_t):
+        for i in ny.arange(0, self.time_frames):
             j = pc.Population(self.main_size, self.square_size, start, self.gauss_width)
             pop_code[:,:,:,i] = j.print_pop(j.initial_pop_code())
             start=ny.add(self.start, self.delta_t)
@@ -76,17 +90,14 @@ class Model(object):
 
         input = self.create_input()
 
-        V1 = Stage([], 100)
-        MT = Stage(get_gauss_kernel(7), 0)
-
         X = ny.zeros((self.main_size, self.main_size, 8, t))
         X[:,:,:, 0] = p.initial_pop_code()
 
         fb = 0
         for d_t in ny.arange(1, t):
-            inp = input[:,:,:,d_t]
-            v1 = V1.do_all(inp, fb)
-            mt = MT.do_all(v1, 0)
+            inp = input[:,:,:,d_t-1]
+            v1 = self.V1.do_all(inp, fb)
+            mt = self.MT.do_all(v1, 0)
             X[:,:,:, d_t] = mt
 
             fb = mt # new feedback is old output
@@ -95,33 +106,30 @@ class Model(object):
 
 
     def integrated_motion_direction(self):
-        self.st = self.start - self.delta_t
-        self.h_v_edges = ny.zeros((self.time_frames/self.delta_t,2))
+        start = self.start
+        self.h_v_edges = ny.zeros((self.time_frames,2))
 
-        X = self.run_model_full (self.time_frames/self.delta_t)
-        print X.shape
+        X = self.run_model_full (self.time_frames)
 
-        for i in ny.arange(0,self.time_frames/self.delta_t):
-            j=pc.Population(self.main_size, self.square_size, self.st+self.delta_t, self.gauss_width)
+        for i in ny.arange(0,self.time_frames):
+            j=pc.Population(self.main_size, self.square_size, start, self.gauss_width)
             pop_c = X[:,:,:,i]
             self.h_v_edges[i,:]=j.create_vectors(pop_c)
+
             pp.plot(i,self.h_v_edges[i,0],'k+')
             pp.plot(i,self.h_v_edges[i,1],'k*')
-            print self.h_v_edges[i,0]
+            start=ny.add(self.start, self.delta_t)
+            print self.h_v_edges[i,0], self.h_v_edges[i,1]
 
-        x=ny.arange(0,self.time_frames/self.delta_t)
+        x=ny.arange(-0.2,self.time_frames)
         y=0*x+45
         pp.plot(x,y)
-        pp.xlim(-0.2,4.2)
+        pp.xlim(-0.2,self.time_frames+0.2)
         pp.ylim(-1,91)
 
 if __name__ == '__main__':
 
-
-    #M=Area(30,6,2,30,10,1)
-    M = Model(30, 6, 2, 30, 10, 1)
+    M = Model('model.cfg')
     M.integrated_motion_direction()
-    #p=pc.Population(30,6,2,30)
-    #M.input(1)
     pp.show()
 
